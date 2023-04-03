9bbf8b056b29e15bb7ec69c09cea5428f87da5dd
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2023-04-03 21:32:17 +0200

    conducted some experiments with 100kbps at home with clicker and firefly from a specified distance of 0.9m and 0.1m alternated the dividers from 18 up until 28

--- baseband/generate-backscatter-pio.py
+++ baseband/generate-backscatter-pio.py
@@ -109,7 +109,7 @@ f'#define PIO_BAUDRATE {b}',
 f'#define PIO_CENTER_OFFSET {round(fcenter*1000)}',
 f'#define PIO_DEVIATION {round(fdeviation*1000)}',
 f'#define PIO_MIN_RX_BW {round((b/1000 + 2*fdeviation)*1000)}', '',
-f'static inline void backscatter_program_init(PIO pio, uint sm, uint offset, uint pin1{", uint pin2" if TWOANTENNAS else ""})' + '{',
+f'static inline void backscatter_program_init(PIO pio, uint sm, uint offset, uint pin1,  uint pin2)' + '{',
  '   pio_gpio_init(pio, pin1);',
  '   pio_sm_set_consecutive_pindirs(pio, sm, pin1, 1, true);'] + ([
  '   pio_gpio_init(pio, pin2);',
--- carrier-receiver-baseband/main.c
+++ carrier-receiver-baseband/main.c
@@ -36,9 +36,9 @@
 #define RECEIVER 2500  // define the receiver board either 2500 or 1352
 #define PIN_TX1 6
 #define PIN_TX2 27
-#define CLOCK_DIV0 30 // larger
-#define CLOCK_DIV1 28 // smaller
-#define DESIRED_BAUD 50000
+#define CLOCK_DIV0 28 // larger
+#define CLOCK_DIV1 26 // smaller
+#define DESIRED_BAUD 100000
 #define TWOANTENNAS true
 
 #define CARRIER_FEQ 2450000000
