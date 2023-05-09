6c90021b80a431337b1070a6fad42d0fb4a362ee
McFrappe <obeyfranso@gmail.com>
2023-05-09 16:31:00 +0200

    feat: deviation and packet len is set correctly
        Co-authored-by: Fredrik Engstrand <fredrik@engstrand.nu>

--- baseband/generate-rf-config.py
+++ baseband/generate-rf-config.py
@@ -6,14 +6,13 @@ from xml.dom.minidom import parse, parseString
 
 CLKFREQ = 125  # MHz
 CARRIER_FREQ = 2450  # MHz
-RF_STUDIO_SYMBOL_RATE_RATIO = 176720286/100000  # RF-studio does weird things
-RF_STUDIO_FREQUENCY_FRAC_RATIO = 39117/5972
+RF_STUDIO_SYMBOL_RATE_RATIO = 176720286/100000  # baud in Bd
+RF_STUDIO_FREQUENCY_FRAC_RATIO = 39117/5972 # offset in MHz
+RF_STUDIO_DEVIATION_RATIO = 3201/100 # deviation is in kHz
 RF_STUDIO_BANDWIDTH_INDEX_OFFSET = 64
-
 
 def to_rf_hex(value):
     return f"0x{format(value, '08x')}"
-
 
 parser = argparse.ArgumentParser(prog="RF-config XML generator",
                                  description="G3 WCNES rf studio config generator\nusage example: python3 generate-rf_config.py 20 18 100000")
@@ -23,11 +22,14 @@ parser.add_argument("divider1", type=int,
                     help=f"clock divider @ {CLKFREQ} MHz for frequency 1 shift; must be an even number e.g. 18 for {(CLKFREQ/18):.3f} Mhz")
 parser.add_argument("baudrate", type=int,
                     help="baud-rate [baud] e.g. 100000 for 100kBaud")
+parser.add_argument("--ecc", action='store_true', default=False,
+                    help="If you are using Error Code Correction")
 
 args = parser.parse_args()
 d0 = args.divider0
 d1 = args.divider1
 b = args.baudrate
+ecc = args.ecc
 
 if (CLKFREQ*(10**6)) % args.baudrate != 0:
     b = round((CLKFREQ*(10**6)) / round((CLKFREQ*(10**6)) / args.baudrate))
@@ -45,6 +47,9 @@ fdeviation = abs(CLKFREQ*1000/d1 - fcenter)
 frequency = CARRIER_FREQ + (fcenter / 1000)
 bandwidth = (b/1000 + 2*fdeviation)
 bandwidth_index = 0
+packet_len = 16
+if ecc:
+    packet_len = 44
 
 available_bandwidths = [
     4.3,  # 64
@@ -103,9 +108,10 @@ print("\nGenerated RF config settings:\n" + "\n".join([
     f"  - frequency 0 shift: {(CLKFREQ/d0):.3f} MHz       (1 period = {d0} cycles @ {CLKFREQ} MHz clock)",
     f"  - frequency 1 shift: {(CLKFREQ/d1):.3f} Mhz       (1 period = {d1} cycles @ {CLKFREQ} MHz clock)",
     f"  - center frequency shift: {(fcenter/1000):.3f} MHz",
-    f"  - deviation from center : {fdeviation:.2f} kHz",
+    f"  - deviation from center: {fdeviation:.2f} kHz",
     f"  - baud-rate {b:.2f}",
     f"  - occupied bandwith: {bandwidth:.2f} kHz",
+    f"  - packet Length: {packet_len}",
     f"",
     f"Saved settings to {out_path}",
 ]), end="\n\n")
@@ -117,6 +123,8 @@ replacements = {
     "FREQUENCY": to_rf_hex(math.floor(frequency)),
     "FREQUENCY_FRAC": to_rf_hex(int((frequency % 1) * 10000 * RF_STUDIO_FREQUENCY_FRAC_RATIO)),
     "CENTER_FREQUENCY": to_rf_hex(math.floor(frequency)),
+    "DEVIATION": to_rf_hex(int(fdeviation * RF_STUDIO_DEVIATION_RATIO)),
+    "PACKET_LENGTH": to_rf_hex(packet_len),
     "PACKET_COUNT": 500,
 }
 
